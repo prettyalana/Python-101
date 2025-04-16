c8d09056154cddb001cf0841e23b7e885c5801b4
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-04-16 14:31:40 -0500

    Add conditional to make sure function runs when script is executed directly

--- level_01/hello_you.py
+++ level_01/hello_you.py
@@ -1,7 +1,11 @@
-# Function to get the name input so I can reuse it for different exercises
+# hello_you
 
+# Function to get the name input so I can reuse it for different exercises
 def get_name_input(): 
-    # hello_you
-    name = input("What is your name?")
+    name = input("What is your name? ")
     print(f"Hello, {name}!")
     return name
+
+# Only run get_name_input() if this file is being run directly and not imported.
+if __name__ == "__main__":
+    get_name_input()
