1130e95c5b791980c831d75f4939a616a88d7f47
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-04-21 01:07:11 -0500

    Define inputs outside of tip_calculator function - Pass in inputs as an argument when the function is called - Pass in bill_total and rating as parameters to tip calculator

--- level_02/tip_calc.py
+++ level_02/tip_calc.py
@@ -1,28 +1,32 @@
 # Tip Calculator
 # TODO: Make this DRY
 
-def tip_calculator() :
-    bill_total = input("Bill total: ")
+# bill_total and rating are parameters (placeholders/variables)
+# bill_total and rating performs the calculations and are passed in when the function is called
+def tip_calculator(bill_total, rating) :
     bill_total = float(bill_total)
-    rating = input(" How was your service: Good, Bad, or Fair? ")
     rating = rating.lower()
 
     good_rating = (20 / 100) * bill_total
     fair_rating = (15 / 100) * bill_total
     bad_rating = (10 / 100) * bill_total
-
-        
+   
     if rating == "good" :
         total_amount = float(bill_total + good_rating)
         # :.2f ensures that the the number is displayed as a floating-point with two decimal places and round up if necessary
-        print(f"${good_rating:.2f}")
+        print(f"Tip amount: ${good_rating:.2f}")
         print(f"Your total is ${total_amount:.2f}")
     elif rating == "fair" :
         total_amount = float(bill_total + fair_rating)
-        print(f"${fair_rating:.2f}")
+        print(f"Tip amount: ${fair_rating:.2f}")
         print(f"Your total is ${total_amount:.2f}")
     else :
         total_amount = float(bill_total + bad_rating)
-        print(f"${bad_rating:.2f}")
+        print(f"Tip amount: ${bad_rating:.2f}")
         print(f"Your total is ${total_amount:.2f}")
-tip_calculator()
+        
+# These inputs are arguments because they are actual values         
+bill_total_input = input("Bill total: ")        
+rating_input = input("How was your service: Good, Bad, or Fair? ")       
+        
+tip_calculator(bill_total_input, rating_input)
--- level_02/tip_calc2.py
+++ level_02/tip_calc2.py
@@ -1,1 +1,5 @@
+from tip_calc import tip_calculator 
 
+
+
+tip_calculator()
